f6e10ea54672bd56b0195a870cc7116d08c541bf
fuartkhana <146414387+fuartkhana@users.noreply.github.com>
2024-01-28 14:38:19 +0400

    ADDED2

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for letter in range(ord('a'), ord('z') + 1):
-	if letter != 'z':
-		print(f"{letter:c}", end = '')
-	else:
-		print(f"{letter:c}", end = '')
+for letter in range(97, 123)
+	print("{}".format(chr(i), end = '')
