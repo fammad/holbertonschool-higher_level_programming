0906f00cf5cd4f4c0d7ce21c5c2605e03a89aa93
Fuad Mammad <fred.mmov@gmail.com>
2024-02-15 12:42:39 -0800

    ADDED 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """2-square.py Defines a Square class with size atributes"""
 
-class Square:
+
+class Square():
     """This Class represent a Square with fixed size
     
     Atributes:
@@ -17,8 +18,8 @@ class Square:
             TypeError: If size is not an integer
             ValueError: If size is less than 0
     """
-        self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
