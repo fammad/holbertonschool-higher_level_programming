ca449be290db5054c567a3c19e867f7ce063f0c5
fuartkhana <146414387+fuartkhana@users.noreply.github.com>
2024-02-18 21:24:06 +0400

    Fixed something

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -2,5 +2,17 @@
 """ Defines Square class"""
 
 
-class Square(self):
-    
+class Square():
+    """Defines class"""
+    def __init__(self, size = 0):
+        """Initialize square object that size of 0"""
+        if isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def size(self):
+    """Method that calculates Area of Square"""
+        return (self.__size ** 2)
+
