8c22cd1fc8f455433298ba791bbb01253ed402b4
Fuad Mammad <fred.mmov@gmail.com>
2024-02-18 10:55:27 -0800

    Added for Test

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -8,13 +8,11 @@ class Square():
         """Initialize square object that size of 0"""
         self.__size = size
         self.__position = position
+
     @property
     def size(self):
         """Getter method that returns private atribute __size"""
         return self.__size
-    def position(self)
-        """Getter for position"""
-        return self.__position
 
     @size.setter
     def size(self, value):
@@ -23,6 +21,20 @@ class Square():
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
+    @property
+    def position(self):
+        """Get position of the Square"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple)
+            and len(value) == 2
+            and value[0] >= 0
+            and value[1] >= 0
+            raise TypeError("position must be a tuple of 2 positive integers")
+            self.__position == value
 
     def area(self):
         return (self.__size ** 2)
