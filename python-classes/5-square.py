65c4d6ab58f4d00d54db33aa606e03dcfc4fcc68
Fuad Mammad <fred.mmov@gmail.com>
2024-02-18 10:03:51 -0800

    ADDED smth

---
+++ python-classes/5-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Defines Square class"""
+
+
+class Square():
+    """Defines class"""
+    def __init__(self, size = 0):
+        """Initialize square object that size of 0"""
+        self.__size = size
+
+    @property
+    def size(self):
+    """Getter method that returns private atribute __size"""
+        return self.__size
+    @size_setter
+    def size(self, value):
+        if isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return (self.__size ** 2)
+
+    def my_print(self):
+        for i in range (0:self.__size):
+            for j in range (0:self.__size):
+                print("#")
