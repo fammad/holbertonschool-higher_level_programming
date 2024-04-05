3438a83d19b2a2628e042f860109521065cef2b0
fuartkhana <146414387+fuartkhana@users.noreply.github.com>
2024-04-05 22:34:14 +0400

    Hello darkness my old friend

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        return self.__width = value
+       self.__width = value
 
     @property
     def height(self):
@@ -33,7 +33,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        return self.__height = value
+        self.__height = value
 
     def area(self):
         return self.width * self.height
