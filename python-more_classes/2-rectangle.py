4c7336f22d15579a9eff589d5c18599dd64fdfe7
fuartkhana <146414387+fuartkhana@users.noreply.github.com>
2024-04-05 22:37:14 +0400

    Hello darkness my old friend

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -15,7 +15,7 @@ class Rectangle:
     def width(self):
         return self.__width
 
-    @setter
+    @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -27,7 +27,7 @@ class Rectangle:
     def height(self):
         return self.__height
 
-    @setter
+    @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
