3cd28b22c7c0de4ac3496ab1cb41325e0bce0307
fuartkhana <146414387+fuartkhana@users.noreply.github.com>
2024-04-05 22:47:16 +0400

    Hello darkness my old friend

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -41,7 +41,7 @@ class Rectangle:
 
     def perimeter(self):
         """Calcualates the perimeter of the object"""
-        if self.width == 0 and self.height == 0:
+        if self.width == 0 or self.height == 0:
             return 0
         else:
             return 2 * (self.width + self.height)
